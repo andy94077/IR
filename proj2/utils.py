a5092af0bd1902ca7928281bd9cb98112396dabd
andy94077 <andy94077@gmail.com>
2020-06-04 16:45:25 +0800

    Init utils.py

---
+++ proj2/utils.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+
+def load_data(path):
+    """Load csv file and return the contents and the number of users and items.
+
+    Args:
+        path: The path of the csv file.
+
+    Returns:
+        positiveX, num_users, num_items,
+        where `positiveX` is a list of lists containing positive samples of each users.
+    """
+
+    df = pd.read_csv(path)
+    positiveX = [[int(i) for i in ll.split()] for ll in df.iloc[:, 1]]
+    max_item_idx = 0
+    for ll in positiveX:
+        max_item_idx = max(ll+[max_item_idx])
+
+    return positiveX, len(positiveX), max_item_idx + 1
