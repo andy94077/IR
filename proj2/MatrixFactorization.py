78861aa2e4c61e1a0eab944b3ab05073556f1ff6
andy94077 <andy94077@gmail.com>
2020-06-06 23:01:29 +0800

    Add input_shape argument in MatrixFactorization.py

--- proj2/MatrixFactorization.py
+++ proj2/MatrixFactorization.py
@@ -6,19 +6,19 @@ from tensorflow.keras import backend as K
 import tensorflow as tf
 
 class MatrixFactorization(Model):
-    def __init__(self, num_users, num_items, latent_dim, regularizer=None, **kwargs):
+    def __init__(self, num_users, num_items, latent_dim, input_shape, regularizer=None, **kwargs):
         super(MatrixFactorization, self).__init__(**kwargs)
         self.user_embedding = Embedding(num_users, latent_dim, embeddings_regularizer=regularizer, name='user_embedding')
         self.item_embedding = Embedding(num_items, latent_dim, embeddings_regularizer=regularizer, name='item_embedding')
+        self.item_count = input_shape[0]
+        self.build((None,) + input_shape)
 
     def call(self, inputs):
-        # print(inputs.shape)
         user_embed = self.user_embedding(inputs[:, 0:1])
         item_embed = self.item_embedding(inputs[:, 1:])
-        # print(user_embed.shape, item_embed.shape)
+        if self.item_count == 3:  # Bayesian personalized ranking
+            item_embed = item_embed[:, 0:1] - item_embed[:, 1:2]
         x = tf.reduce_sum(tf.math.multiply(user_embed, item_embed), axis=-1)
-        if inputs.shape[1] == 3:  # Bayesian personalized ranking
-            x = x[:, 0:1] - x[:, 1:2]
         x = tf.nn.sigmoid(x)
 
         return x
